2438cae2140266f7dfe1ea8bff7ba9506f6335ce
Tau <tau@aello.dev>
2019-09-04 21:24:38 -0400

    Add getInstance()

--- src/main/java/dev/aello/deathspawn/DeathSpawn.java
+++ src/main/java/dev/aello/deathspawn/DeathSpawn.java
@@ -4,9 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class DeathSpawn extends JavaPlugin
 {
+    private static DeathSpawn instance;
+
     @Override
     public void onEnable()
     {
+        instance = this;
         saveDefaultConfig();
+    }
+
+    public static DeathSpawn getInstance()
+    {
+        return instance;
     }
 }
