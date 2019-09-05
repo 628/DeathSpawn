ce05b11836a2a3a3407ef37886ac8bf3d7001429
Tau <tau@aello.dev>
2019-09-04 21:11:04 -0400

    Initial commit

---
+++ src/main/java/dev/aello/deathspawn/DeathSpawn.java
@@ -0,0 +1,12 @@
+package dev.aello.deathspawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathSpawn extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+}
